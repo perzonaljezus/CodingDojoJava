f495507d5d4c973c34baf822d853863a7fda5dfd
pj <a@b.com>
2015-11-29 22:38:04 +0100

    roman numbers subtract: major refactoring

--- src/main/RomanNumbersSubstract/RomanNumerals.java
+++ src/main/RomanNumbersSubstract/RomanNumerals.java
@@ -1,5 +1,6 @@
 package RomanNumbersSubstract;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -40,228 +41,266 @@ werden generell nicht in subtraktiver Stellung
 einem größeren Zeichen vorangestellt.
  */
 public class RomanNumerals {
-    TreeMap<Integer,String> romanNumbers = new TreeMap<Integer,String>();
+    private TreeMap<Integer,String> romanNumbers;
+    private final Map<Integer, Block> blocks;
+
+    private int remainder;
+
     public RomanNumerals() {
+        romanNumbers = new TreeMap<Integer,String>();
+        //
         romanNumbers.put(1000,"M");
-        romanNumbers.put(500,"D");
+        romanNumbers.put(500,"D"); // middle between 1000 and 100
         romanNumbers.put(100,"C");
-        romanNumbers.put(50,"L");
+        romanNumbers.put(50,"L"); // middle between 100 and 10
         romanNumbers.put(10,"X");
-        romanNumbers.put(5,"V");
+        romanNumbers.put(5,"V"); // middle between 10 and 1
         romanNumbers.put(1,"I");
-    }
-    Map<Integer,String> descendingRomanNumbers = romanNumbers.descendingMap();
+        //
+        romanNumbers.put(0,""); // not nice: we need this only to get addToBaseValue working correctly for 1000s
 
-    public String convertArabicNumber(int arabic) {
-        int i = arabic;
-
-        String result = "";
-
-        int thousands = getThousands(arabic);
-        String thousandsConverted = convertThousands(thousands);
-        int remainder = arabic - 1000 * thousands;
-
-        int hundreds = getHundreds(remainder);
-        String hundredsConverted = convertHundreds(hundreds);
-        remainder = remainder - 100 * hundreds;
-
-        int tens = getTens(remainder);
-        String tensConverted = convertTens(tens);
-        remainder = remainder - 10 * tens;
-
-        int ones = getOnes(remainder);
-        String onesConverted = convertOnes(ones);
-
-        return thousandsConverted
-                + hundredsConverted
-                + tensConverted
-                + onesConverted;
+        // the reverse order is important, the algorithm in convertArabicNumber expects it descending!
+        blocks = new TreeMap<>(Collections.reverseOrder());
+        blocks.put(1000, new Block(10000, 5000, 1000));
+        blocks.put(100, new Block(1000, 500, 100));
+        blocks.put(10, new Block(100, 50, 10));
+        blocks.put(1, new Block(10, 5, 1));
     }
 
-    public String convertThousands(int thousands) {
-        String result = "";
-        for(int i=1; i <= thousands; i++ ) {
-            result += "M";
+    // main
+    public String convertArabicNumber(int arabicNumber) throws IllegalArgumentException {
+
+        // contract: we operate only in the specified range
+        if( arabicNumber < 1 || arabicNumber > 3000) {
+            throw new IllegalArgumentException();
         }
-        return result;
+
+        //convertedArabicNumber will be a String built by adding
+        //          thousandsConverted
+        //        + hundredsConverted
+        //        + tensConverted
+        //        + onesConverted
+        String convertedArabicNumber = "";
+
+        // loop through the digits of the arabic number and convert to roman number
+        int tmpArabicNumer = arabicNumber;
+        for(Block b: blocks.values() ) {
+
+            int arabicDigit        = getDigitAtPlace(tmpArabicNumer, b.lowerLimit);
+            convertedArabicNumber += convertArabicDigit(arabicDigit, b.upperLimit, b.middleLimit, b.lowerLimit);
+
+            tmpArabicNumer         = getRemainder(tmpArabicNumer, b.lowerLimit);
+        }
+
+        if( tmpArabicNumer != 0) {
+            throw new RuntimeException("tmpArabicNumber must be 0 after converting");
+        }
+
+        return convertedArabicNumber;
     }
 
-    public int getThousands(int i) {
-        return getNumber(i, 1000);
+
+    // ---------------------------------------------------------
+
+    // business logic / domain method names
+    // used in test -> we could remove the tests and remove these methods.
+
+    /**
+     * returns the roman numeral representing "thousands * 1000"
+     * e.g. thousands = 2 => 2*1000 => MM
+     * @param int thousands
+     * @return String
+     */
+    public String convertThousands(int thousands) {
+        Block b = getBlock1000();
+        return convertArabicDigit(thousands, b.upperLimit, b.middleLimit, b.lowerLimit);
     }
 
     public String convertHundreds(int hundreds) {
-        if( hundreds == 0) {
-            return "";
-        }
-
-        String result = "";
-        int distanceFrom1000 = 10 - hundreds;
-        int distanceFrom500  =  hundreds - 5;
-
-        if( hundreds >= 5 ) {
-            if( distanceFrom1000 == 1 && distanceFrom1000 < distanceFrom500) {
-                // subtract from 1000
-                result = "";
-                for(int i=1; i<= distanceFrom1000; i++) {
-                    result += "C";
-                }
-                result += "M";
-            } else {
-                // add to 500
-                result = "D";
-                for(int i=1; i<= distanceFrom500; i++) {
-                    result += "C";
-                }
-            }
-        } else {
-            distanceFrom500  =  5 - hundreds;
-            int distanceFrom100  =  hundreds - 1;
-            if( distanceFrom500 < distanceFrom100) {
-                // subtract from 500
-                result = "";
-                for(int i=1; i<= distanceFrom500; i++) {
-                    result += "C";
-                }
-                result += "D";
-            } else {
-                // add 100s
-                result = "C";
-                for(int i=1; i<= distanceFrom100; i++) {
-                    result += "C";
-                }
-            }
-        }
-
-        return result;
+        Block b = getBlock100();
+        return convertArabicDigit(hundreds, b.upperLimit, b.middleLimit, b.lowerLimit);
     }
-
-    public int getHundreds(int i) {
-        // contract: i must be in the range [1000;3000]
-        if(i>=1000 && i<=3000) {
-            throw new IllegalArgumentException();
-        }
-        return getNumber(i, 100);
-    }
-
-
 
     public String convertTens(int tens) {
-        if( tens == 0) {
-            return "";
-        }
-
-        String result = "";
-        int distanceFrom100 = 10 - tens;
-        int distanceFrom50  =  tens - 5;
-
-        if( tens >= 5 ) {
-            if( distanceFrom100 == 1 && distanceFrom100 < distanceFrom50) {
-                // subtract from 100
-                result = "";
-                for(int i=1; i<= distanceFrom100; i++) {
-                    result += "X";
-                }
-                result += "C";
-            } else {
-                // add to 50
-                result = "L";
-                for(int i=1; i<= distanceFrom50; i++) {
-                    result += "X";
-                }
-            }
-        } else {
-            distanceFrom50  =  5 - tens;
-            int distanceFrom10  =  tens - 1;
-            if( distanceFrom50 == 1 && distanceFrom50 < distanceFrom10) {
-                // subtract from 50
-                result = "";
-                for(int i=1; i<= distanceFrom50; i++) {
-                    result += "X";
-                }
-                result += "L";
-            } else {
-                // add 10s
-                result = "X";
-                for(int i=1; i<= distanceFrom10; i++) {
-                    result += "X";
-                }
-            }
-        }
-
-        return result;
+        Block b = getBlock10();
+        return convertArabicDigit(tens, b.upperLimit, b.middleLimit, b.lowerLimit);
     }
-
-    public int getTens(int i) {
-        // contract: i must in the range [99;10]
-        if( i<=99 && i>=10) {
-            return getNumber(i, 10);
-        }
-        return 0;
-    }
-
 
     public String convertOnes(int ones) {
-        if( ones == 0) {
+        Block b = getBlock1();
+        return convertArabicDigit(ones, b.upperLimit, b.middleLimit, b.lowerLimit);
+    }
+    // discussion: make these helper methods protected, so that the main useful method is the only public method (convertArabicNumber)
+    // or make them private, but then we can't test directly
+
+
+    // ----------------------------------------------------------
+
+    // non domain -> implementation logic
+
+    private String convertArabicDigit(int arabicDigitValue, int upperLimitOfBlock, int middleLimitOfBlock, int lowerLimitOfBlock) {
+        if( arabicDigitValue == 0) {
             return "";
         }
 
         String result = "";
-        int distanceFrom10 = 10 - ones;
-        int distanceFrom5  =  ones -5;
+        int distanceFromUppderLimit = 10 - arabicDigitValue;
+        int distanceFromMiddleLimit  = arabicDigitValue - 5;
 
-        if( ones >= 5 ) {
-            if( distanceFrom10 == 1 && distanceFrom10 < distanceFrom5) {
-                // subtract from 10
-                result = "";
-                for(int i=1; i<= distanceFrom10; i++) {
-                    result += "I";
-                }
-                result += "X";
-            } else {
-                // add to 5
-                result = "V";
-                for(int i=1; i<= distanceFrom5; i++) {
-                    result += "I";
-                }
-            }
+        if( arabicDigitValue >= 5 ) { // if number of tens further from the middle value
+            result = computeRomanValueUpper(upperLimitOfBlock, middleLimitOfBlock, lowerLimitOfBlock, distanceFromUppderLimit, distanceFromMiddleLimit, distanceFromMiddleLimit);
         } else {
-            distanceFrom5  =  5 - ones;
-            int distanceFrom1  =  ones - 1;
-            if( distanceFrom5 == 1 && distanceFrom5 < distanceFrom10) {
-                // subtract from 5
-                result = "";
-                for(int i=1; i<= distanceFrom5; i++) {
-                    result += "I";
-                }
-                result += "V";
-            } else {
-                // add 1s
-                result = "I";
-                for(int i=1; i<= distanceFrom1; i++) {
-                    result += "I";
-                }
-            }
+            distanceFromMiddleLimit  =  5 - arabicDigitValue;
+            int distanceFromLowerLimit  =  arabicDigitValue - 1;
+            result = computeRomanValueLower(upperLimitOfBlock, middleLimitOfBlock, lowerLimitOfBlock, distanceFromUppderLimit, distanceFromMiddleLimit, distanceFromLowerLimit);
         }
 
         return result;
     }
 
-    public int getOnes(int i) {
-        // contract: i must in the range [9;1]
-        if(i<=9 && i>=0) {
-            return getNumber(i, 1);
+    private String computeRomanValueUpper(int upperLimit, int baseValue, int lowerLimit, int distanceOfBaseValueFromUpperLimit, int distanceOfBaseValueFromMiddleLimit , int distanceFromMiddleLimit) {
+        String result;
+        if( distanceOfBaseValueFromUpperLimit == 1 // subtraction: subtract the lower value only once => distance == 1
+                && distanceOfBaseValueFromUpperLimit < distanceOfBaseValueFromMiddleLimit) {
+            result = subtractFromBaseValue(upperLimit, lowerLimit, distanceOfBaseValueFromUpperLimit);
+        } else {
+            result = addToBaseValue(baseValue, lowerLimit, distanceOfBaseValueFromMiddleLimit);
         }
-        return 0;
+        return result;
+    }
+
+    private String computeRomanValueLower(int upperLimit, int baseValue, int lowerLimitOfBlock, int distanceFromUpperLimit, int distanceOfBaseValueFromMiddleLimit, int distanceFromLowerLimit) {
+        String result;
+        if( distanceOfBaseValueFromMiddleLimit == 1
+                && distanceOfBaseValueFromMiddleLimit < distanceFromLowerLimit) {
+            result = subtractFromBaseValue(baseValue, lowerLimitOfBlock, distanceOfBaseValueFromMiddleLimit);
+        } else {
+            result = addToBaseValue(lowerLimitOfBlock, lowerLimitOfBlock, distanceFromLowerLimit);
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param i arabic number
+     * @param place position in arabic number
+     * @return int the digit at the specified position
+     */
+    private int getDigitAtPlace(int i, int place) {
+        return getQuotient(i, place);
+    }
+
+    /**
+     *
+     * @param baseValue
+     * @param lowerValue
+     * @param distanceOfLowerValueFromBaseValue
+     * @return roman numeral expression
+     */
+    private String subtractFromBaseValue(int baseValue, int lowerValue, int distanceOfLowerValueFromBaseValue) {
+        String result = "";
+        for(int i=1; i<= distanceOfLowerValueFromBaseValue; i++) {
+            result += romanNumbers.get(lowerValue);
+        }
+        result += romanNumbers.get(baseValue);
+        return result;
+    }
+
+    /**
+     *
+     * @param baseValue
+     * @param lowerValue
+     * @param distanceFromBaseValue
+     * @return roman numeral expression
+     */
+    private String addToBaseValue(int baseValue, int lowerValue, int distanceFromBaseValue) {
+        String result = romanNumbers.get(baseValue);
+        for(int i = 1; i<= distanceFromBaseValue; i++) {
+            result += romanNumbers.get(lowerValue);
+        }
+        return result;
     }
 
 
+    // discussion: I made this private. why?
 
-
-    // -----
-
-    public int getNumber(int i, int place) {
+    private int getQuotient(int i, int place) {
         return (int) i / place;
     }
 
+    /**
+     *
+     * @param placeValue
+     * @param initialValue
+     * @return int new remainder
+     */
+    private int getRemainder( int initialValue, int placeValue ) {
+        return (int) initialValue % placeValue;
+    }
+
+    /**
+     * Roman numerals have an interior structure of "blocks"
+     * e.g. the 1000 block contains 1000, 500 and 100.
+     * Because, if we want to convert an arabic value of 654 we see that 600 is in the upper half
+     * of the block (600 is in the range 1000 - 500).
+     * 50 is in the block 100, 50, 10 => 50 is the lowest number of the "upper" half i.e. the range 100 - 50.
+     * and
+     * 4 is in the block 10, 5, 1 => 4 is in the "lower" half of the block.
+     *
+     * To convert e.g. 321, we see that 300 is in the range 500 - 100, the "lower" half of the block 1000,500,100.
+     *
+     * In order to compute roman numbers in the subtracting / additive way, we have to know these blocks and the boundaries
+     * at which we have to change the base value of the computation.
+     *
+     * In the example above: 654 has the baseValue 500, and we add 100 to compute the roman representation.
+     */
+    class Block {
+        int upperLimit;
+        int middleLimit;
+        int lowerLimit;
+
+        public Block(int upper, int middle, int lower) {
+            this.upperLimit = upper;
+            this.middleLimit = middle;
+            this.lowerLimit = lower;
+
+        }
+        public void addUpperLimit(int limit) {
+            this.upperLimit = limit;
+        }
+        public void addMiddleLimit(int limit) {
+            this.middleLimit = limit;
+        }
+        public void addLowerLimit(int limit) {
+            this.lowerLimit = limit;
+        }
+
+        public int getUpperLimit() {
+            return this.upperLimit;
+        }
+        public int getMiddleLimit() {
+            return this.middleLimit;
+        }
+        public int getLowerLimit() {
+            return this.lowerLimit;
+        }
+    }
+
+    private Map getBlocks() {
+        return this.blocks;
+    }
+
+    private Block getBlock1000() {
+        return this.blocks.get(1000);
+    }
+    private Block getBlock100() {
+        return this.blocks.get(100);
+    }
+    private Block getBlock10() {
+        return this.blocks.get(10);
+    }
+    private Block getBlock1() {
+        return this.blocks.get(1);
+    }
 }
+
--- src/test/RomanNumbersSubstract/RomanNumeralsTest.java
+++ src/test/RomanNumbersSubstract/RomanNumeralsTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralsTest {
-    RomanNumerals r = null;
+
+    RomanNumerals r;
 
     @Before
     public void setUp() throws Exception {
@@ -14,84 +15,68 @@ public class RomanNumeralsTest {
     }
 
     // Acceptance Tests
-    @Test
-    public void test1990toMCMXC() throws Exception {
-        assertEquals("MCMXC", r.convertArabicNumber(1990));
-    }
-    @Test
-    public void test1984toMCMLXXXIV() throws Exception {
-        assertEquals("MCMLXXXIV", r.convertArabicNumber(1984));
-    }
 
     @Test
-    public void test2008toMMVIII() throws Exception {
-        assertEquals("MMVIII", r.convertArabicNumber(2008));
-    }
+    public void test207toCCVII() throws Exception { assertEquals("CCVII", r.convertArabicNumber(207));}
     @Test
-    public void test1234toMCCXXXIV() throws Exception {
-        assertEquals("MCCXXXIV", r.convertArabicNumber(1234));
-    }
+    public void test1066toMLXVI() throws Exception { assertEquals("MLXVI", r.convertArabicNumber(1066));}
     @Test
-    public void test987toCMLXXXVII() throws Exception {
-        assertEquals("CMLXXXVII", r.convertArabicNumber(987));
+    public void test1954toMCMLIV() throws Exception { assertEquals("MCMLIV", r.convertArabicNumber(1954));}
+    @Test
+    public void test1990toMCMXC() throws Exception { assertEquals("MCMXC", r.convertArabicNumber(1990));}
+    @Test
+    public void test1984toMCMLXXXIV() throws Exception { assertEquals("MCMLXXXIV", r.convertArabicNumber(1984));}
+    @Test
+    public void test2014toMMXIV() throws Exception { assertEquals("MMXIV", r.convertArabicNumber(2014));}
+
+    @Test
+    public void test2008toMMVIII() throws Exception { assertEquals("MMVIII", r.convertArabicNumber(2008));}
+    @Test
+    public void test1234toMCCXXXIV() throws Exception { assertEquals("MCCXXXIV", r.convertArabicNumber(1234));}
+    @Test
+    public void test987toCMLXXXVII() throws Exception { assertEquals("CMLXXXVII", r.convertArabicNumber(987));}
+    @Test
+    public void test27toXXVII() throws Exception { assertEquals("XXVII", r.convertArabicNumber(27));}
+
+
+    @Test
+    public void test1toI() throws Exception { assertEquals("I", r.convertArabicNumber(1)); }
+    @Test
+    public void test2toII() throws Exception { assertEquals("II", r.convertArabicNumber(2));}
+    @Test
+    public void test3toIII() throws Exception { assertEquals("III", r.convertArabicNumber(3));}
+    @Test
+    public void test4toIV() throws Exception { assertEquals("IV", r.convertArabicNumber(4));}
+    @Test
+    public void test5toV() throws Exception { assertEquals("V", r.convertArabicNumber(5));}
+    @Test
+    public void test6toVI() throws Exception { assertEquals("VI", r.convertArabicNumber(6));}
+    @Test
+    public void test7toVII() throws Exception { assertEquals("VII", r.convertArabicNumber(7));}
+    @Test
+    public void test8toVIII() throws Exception { assertEquals("VIII", r.convertArabicNumber(8));}
+    @Test
+    public void test9toIX() throws Exception { assertEquals("IX", r.convertArabicNumber(9));}
+    @Test
+    public void test10toX() throws Exception { assertEquals("X", r.convertArabicNumber(10));}
+
+
+    // illegal values
+    @Test(expected = IllegalArgumentException.class)
+    public void test9987throwsException() throws Exception {
+        assertEquals("SHOULDN'T ARRIVE HERE!", r.convertArabicNumber(9987), "SHOULDN'T ARRIVE HERE!");
     }
+
 
     // unit tests
 
-    // Roman numbers
-    @Test
-    public void test1toI() throws Exception {
-        assertEquals("I", r.convertArabicNumber(1));
-    }
-    @Test
-    public void test2toII() throws Exception {
-        assertEquals("II", r.convertArabicNumber(2));
-    }
-    @Test
-    public void test3toIII() throws Exception {
-        assertEquals("III", r.convertArabicNumber(3));
-    }
-    @Test
-    public void test4toIV() throws Exception {
-        assertEquals("IV", r.convertArabicNumber(4));
-    }
-    @Test
-    public void test5toV() throws Exception {
-        assertEquals("V", r.convertArabicNumber(5));
-    }
-    @Test
-    public void test6toVI() throws Exception {
-        assertEquals("VI", r.convertArabicNumber(6));
-    }
-    @Test
-    public void test7toVII() throws Exception {
-        assertEquals("VII", r.convertArabicNumber(7));
-    }
-    @Test
-    public void test8toVIII() throws Exception {
-        assertEquals("VIII", r.convertArabicNumber(8));
-    }
-    @Test
-    public void test9toIX() throws Exception {
-        assertEquals("IX", r.convertArabicNumber(9));
-    }
-    @Test
-    public void test10toX() throws Exception {
-        assertEquals("X", r.convertArabicNumber(10));
-    }
-
     // helper methods
 
-    // convert
+    @Test
+    public void testConvert2Thousands() {assertEquals("MM",r.convertThousands(2));}
 
     @Test
-    public void testConvert2Thousands() {
-        assertEquals("MM",r.convertThousands(2));
-    }
-    @Test
-    public void testConvert9Hundreds() {
-        assertEquals("CM",r.convertHundreds(9));
-    }
+    public void testConvert9Hundreds() { assertEquals("CM",r.convertHundreds(9));}
     @Test
     public void testConvert8Hundreds() {
         assertEquals("DCCC",r.convertHundreds(8));
@@ -116,6 +101,7 @@ public class RomanNumeralsTest {
     public void testConvert1Hundreds() {
         assertEquals("C",r.convertHundreds(1));
     }
+
     @Test
     public void testConvert9Tens() {
         assertEquals("XC",r.convertTens(9));
@@ -128,7 +114,6 @@ public class RomanNumeralsTest {
     public void testConvert7Tens() {
         assertEquals("LXX",r.convertTens(7));
     }
-
     @Test
     public void testConvert5Tens() {
         assertEquals("L",r.convertTens(5));
@@ -147,8 +132,6 @@ public class RomanNumeralsTest {
     }
 
 
-
-
     @Test
     public void testConvert9Ones() {
         assertEquals("IX",r.convertOnes(9));
@@ -161,7 +144,6 @@ public class RomanNumeralsTest {
     public void testConvert7Ones() {
         assertEquals("VII",r.convertOnes(7));
     }
-
     @Test
     public void testConvert5Ones() {
         assertEquals("V",r.convertOnes(5));
@@ -175,29 +157,6 @@ public class RomanNumeralsTest {
         assertEquals("III",r.convertOnes(3));
     }
     @Test
-    public void testConvert1Ones() {
-        assertEquals("I",r.convertOnes(1));
-    }
-
-
-    // get
-
-    @Test
-    public void testGetThousands2345() {
-        assertEquals(2,r.getThousands(2345));
-    }
-    @Test
-    public void testGetThousands1987() {
-        assertEquals(1,r.getThousands(1987));
-    }
-    @Test
-    public void testGetHundreds987() {
-        assertEquals(9,r.getHundreds(987));
-    }
-    @Test
-    public void testGetTens87() {
-        assertEquals(8,r.getTens(87));
-    }
-
+    public void testConvert1Ones() { assertEquals("I",r.convertOnes(1));}
 
 }
